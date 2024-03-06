04e0cd82dff9d498fe09e847271a9aad5203321e
matzgans <rahmatlahay84@gmail.com>
2024-03-06 13:54:29 +0800

    last commit from jr

--- resources/views/chat/index.blade.php
+++ resources/views/chat/index.blade.php
@@ -5,8 +5,28 @@
         </h2>
     </x-slot>
     <div class="mt-4">
+        @foreach ($chats as $chat)
+            <div class="pt-3">
+                <div
+                    class="max-w-3xl mx-auto sm:px-6 lg:px-8 bg-white py-3 flex rounded-lg shadow-md hover:bg-slate-50 ">
+                    <div class="w-auto">
 
-        <div class="pt-3">
+                        <div class="bg-black w-16 h-16 rounded-full me-4"></div>
+                    </div>
+                    <div class="">
+                        <h2 class="text-black text-2xl font-medium">{{ $chat->user->name }}</h2>
+                        <p class="text-gray-400 line-clamp-1">{{ $chat->chatMessages->first()->message }}</p>
+
+                    </div>
+                    <div class="ms-auto text-gray-400">
+                        12.00
+                    </div>
+
+                </div>
+            </div>
+        @endforeach
+
+        {{-- <div class="pt-3">
             <div class="max-w-3xl mx-auto sm:px-6 lg:px-8 bg-white py-3 flex rounded-lg shadow-md hover:bg-slate-50 ">
                 <div class="w-auto">
 
@@ -25,7 +45,7 @@
                 </div>
 
             </div>
-        </div>
+        </div> --}}
         {{-- <div class="pt-3">
             <div class="max-w-3xl mx-auto sm:px-6 lg:px-8 bg-white py-3 flex rounded-lg shadow-md">
                 <div class="w-auto">
