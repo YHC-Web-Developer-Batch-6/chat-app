6c235c1d6ebdc8b0b156cd0e2593c521ae1459b1
matzgans <rahmatlahay84@gmail.com>
2024-03-07 14:45:12 +0800

    from rahmat

--- app/Http/Controllers/ChatMessageController.php
+++ app/Http/Controllers/ChatMessageController.php
@@ -27,9 +27,8 @@ class ChatMessageController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request): 
+    public function store(Request $request)
     {
-
     }
 
     /**
