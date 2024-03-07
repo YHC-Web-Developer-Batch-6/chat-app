9d42b72bdec9c746209b213eca714f96f9d3a74b
rosyanxone <rausyanfikrkarmayoga@gmail.com>
2024-03-07 15:52:33 +0800

    notify new message

--- resources/views/chat/index.blade.php
+++ resources/views/chat/index.blade.php
@@ -6,29 +6,50 @@
     </x-slot>
     <div class="mt-4">
         @foreach ($chats as $chat)
-            @if ($chat->user_id != auth()->user()->id)
-                <a href="{{ route('chat.message', ['id' => $chat->chat_room_id]) }}">
-                    <div class="pt-3">
-                        <div
-                            class="max-w-3xl mx-auto sm:px-6 lg:px-8 bg-white py-3 flex rounded-lg shadow-md hover:bg-slate-50 ">
-                            <div class="w-auto">
-                                <div class="bg-black w-16 h-16 rounded-full me-4 overflow-hidden">
-                                    <img src="https://source.unsplash.com/800x800?person" alt="Profile Pict">
-                                </div>
-                            </div>
-                            <div class="">
-                                <h2 class="text-black text-2xl font-medium">{{ $chat->user->name }}</h2>
-                                <p class="text-gray-400 line-clamp-1">{{ $chat->chatRoom->last_message }}</p>
-                            </div>
-                            <div class="ms-auto text-gray-400">
-                                12.00
+            <a href="{{ route('chat.message', ['id' => $chat->chat_room_id]) }}">
+                <div id="room-chat-{{ $chat->chat_room_id }}" class="pt-3">
+                    <div
+                        class="max-w-3xl mx-auto sm:px-6 lg:px-8 bg-white py-3 flex rounded-lg shadow-md hover:bg-slate-50 ">
+                        <div class="w-auto">
+                            <div class="bg-black w-16 h-16 rounded-full me-4 overflow-hidden">
+                                <img src="https://source.unsplash.com/800x800?person" alt="Profile Pict">
                             </div>
                         </div>
+                        <div class="">
+                            <h2 class="text-black text-2xl font-medium">{{ $chat->user->name }}</h2>
+                            <p class="text-gray-400 line-clamp-1">{{ $chat->chatRoom->last_message }}</p>
+                        </div>
+                        <div class="ms-auto text-gray-400">
+                            12.00
+                        </div>
                     </div>
-                </a>
-            @endif
+                </div>
+            </a>
         @endforeach
     </div>
 
+    <script>
+        // Enable pusher logging - don't include this in production
+        Pusher.logToConsole = true;
 
+        const roomChatIds = {!! json_encode($chats->pluck('chat_room_id')->toArray()) !!};
+        const token = document.querySelector('meta[name="csrf-token"]').content;
+        const pusher = new Pusher('{!! env('PUSHER_APP_KEY') !!}', {
+            cluster: "ap1",
+            channelAuthorization: {
+                endpoint: "/broadcasting/auth",
+                headers: {
+                    "X-CSRF-Token": token
+                },
+            },
+        });
+
+        roomChatIds.forEach(id => {
+            const channel = pusher.subscribe(`private-room.channel.${id}`);
+            
+            channel.bind("chat-message-event", function(data) {
+                alert("New Message");
+            });
+        });
+    </script>
 </x-app-layout>
